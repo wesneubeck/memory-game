c967bac8a970d7e4435581f4e2c078c7541d2dfb
Wes <wesneubeck@gmail.com>
2018-09-14 17:31:46 -0400

    add html, css, javascript files

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+                        
+function flipCard() { 
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    
+        return;
+    }
+    
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch(); 
+}
+
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+    isMatch ? disableCards() : unFlipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards() {    
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1000);
+} 
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+// then apply to each memory card once click - flipCard() will fire
